41032e24ce39a9b1d2018b8bf5f2335d817fa53a
Danner Abel Aly <dannerabelaly@gmail.com>
2023-11-21 19:59:34 -0400

    estos son los colores

--- app/src/main/java/com/example/shei145botonesabajo/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/shei145botonesabajo/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Desarrolladores"
+        value = "Mas opciones"
     }
     val text: LiveData<String> = _text
 }
