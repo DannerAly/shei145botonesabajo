636f19a3cf822680c8fc151f7345762fbe11bfc5
Danner Abel Aly <dannerabelaly@gmail.com>
2023-11-21 19:42:48 -0400

    ESTA ES UNA MODIFICACION

--- app/src/main/java/com/example/shei145botonesabajo/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/shei145botonesabajo/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Vive el momento"
     }
     val text: LiveData<String> = _text
 }
