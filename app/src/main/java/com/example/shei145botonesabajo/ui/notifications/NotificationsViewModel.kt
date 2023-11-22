c201bb069f74f0ce0b55790d0d155faf8764fe63
Sheila-RV <shei.rv121@gmail.com>
2023-11-22 01:00:32 -0400

    desarrolladores
    Desarrolladores

--- app/src/main/java/com/example/shei145botonesabajo/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/example/shei145botonesabajo/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Mas opciones"
+        value = "Desarrolladores"
     }
     val text: LiveData<String> = _text
 }
